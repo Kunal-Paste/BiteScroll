80914a138efdad4b71082f8853fa76756dd5d14f
Kunal-Paste <pastekunal6@gamail.com>
2025-09-24 20:24:26 +0530

    Add user and food partner authentication pages with routing

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import UserRegister from '../pages/auth/UserRegister'
+import UserLogin from '../pages/auth/UserLogin'
+import FoodPartnerRegister from '../pages/auth/FoodPartnerRegister'
+import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin'
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/user/register' element={<UserRegister />} />
-                <Route path='/user/login' element={<h1>register221</h1>} />
-                <Route path='/food-partner/register' element={<h1>register1wqr</h1>} />
-                <Route path='/food-partner/login' element={<h1>register1wrqr</h1>} />
+                <Route path='/user/login' element={<UserLogin/>} />
+                <Route path='/food-partner/register' element={<FoodPartnerRegister/>} />
+                <Route path='/food-partner/login' element={<FoodPartnerLogin/>} />
             </Routes>
         </Router>
     )
