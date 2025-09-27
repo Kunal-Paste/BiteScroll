8f38fe3da4e5f3cc7d672a6f23e9e19658203092
Kunal-Paste <pastekunal6@gamail.com>
2025-09-27 19:16:22 +0530

    Add font licenses, index HTML, and Vite SVG
    - Added license file for "Badockery Display Food Font" indicating it's free for personal use.
    - Included license information for "MUST READ" font, emphasizing personal use only and commercial licensing details.
    - Created a basic index.html file for the frontend with necessary meta tags and links to scripts and styles.
    - Added Vite SVG logo file for use in the application.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,7 @@ const authRoute = require('./routes/auth.routes');
 const foodRoute = require('./routes/food.routes');
 const foodPartnerRoute = require('./routes/food-partner.routes')
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors({
@@ -12,9 +13,14 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'../public')));
 
 app.use('/api/auth', authRoute);
 app.use('/api/food', foodRoute);
 app.use('/api/food-partner', foodPartnerRoute);
+
+app.get("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
 
 module.exports = app;
